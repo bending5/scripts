f7b883cc0e747788af5429bd29e8e8bca621430c
bendig5 <mdi.pochta@mail.ru>
2023-08-27 21:46:18 +0300

    Add fonts to environment as of 2023-08-27

---
+++ nerdfonts.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+sudo apt install unzip -y
+
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+fonts=( 
+"Hack"  
+)
+
+for font in ${fonts[@]}
+do
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip
+	unzip $font.zip -d $HOME/.local/share/fonts/$font/
+    rm $font.zip
+done
+fc-cache
