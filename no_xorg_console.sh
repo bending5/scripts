e4922be01766f58c0d41de6c3f6e335c62799383
bending5 <mdi.pochta@mail.ru>
2023-09-09 05:51:26 +0300

    Install font for raw terminal

---
+++ no_xorg_console.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install font capabale to change its sizge
+sudo apt-get install xfonts-terminus -y
+
+# run this to setup new font
+# sudo dpkg-reconfigure -plow console-setup
